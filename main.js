55c4397c173ecf55bdfa3a569fc95f12cf85fb24
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-07-25 00:27:54 +0200

    implement getOptioons method in ctor

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ class Minecraft {
     /**
      * Create a new Minecraft instance
      * @param {string} version version name
-     * @param {object} options configuration options from options.txt
+     * @param {string} root .minecraft dir location
      * @param {Chat} chat Instance of Chat
      * @param {Command[]} commands available commands
      */
-    constructor(version, options, chat, commands) {
+    constructor(version, root, chat, commands) {
         this.version = version;
-        this.options = options;
+        this.options = this.getOptions(`${root}/options.txt`);
         this.chat = chat;
         this.commands = commands;
     }
