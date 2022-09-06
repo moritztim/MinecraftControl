dd1e5022c7ff654a4cb58961722b17e31788a996
moritz-t-w <90388353+moritz-t-w@users.noreply.github.com>
2022-09-07 00:56:09 +0200

    make commands optional

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ class Minecraft {
         }
         this.options = Minecraft.getOptions(root + optionsFilePath);
         if (version != undefined) this.version = version;
-        this.chat = chat ?? new Chat(this.options['key_key.chat'], this.options['key_key.command'], defaultPrefix);
+        this.chat = chat ?? new Chat(this.options['key_key.chat'], this.options['key_key.command'], defaultPrefix]);
     }
 
     /**
@@ -74,7 +74,7 @@ class Chat {
 	key: string;
 	private commandKey: string;
 	private commandPrefix: string;
-	commands: Command[];
+	commands?: Command[];
     /**
      * Create a new Chat instance
      * @param {string} [key] the key to open the chat
@@ -82,7 +82,7 @@ class Chat {
      * @param {string} [commandPrefix] the command prefix
      * @param {Command[]} [commands] available commands
      */
-    constructor(key: string, commandKey: string, commandPrefix: string = '/', commands: Command[]) {
+    constructor(key: string, commandKey: string, commandPrefix: string = '/', commands?: Command[]) {
         let defaultOptions = Minecraft.getOptions('./default.options.txt');
         this.key = key ?? defaultOptions['key_key.chat'];
         this.commandKey = commandKey ?? defaultOptions['key_key.command'];
